f4a899e20139df2bbb2e055d501ffeef63fb12aa
SchwiftyUI <schwiftyui@gmail.com>
2020-01-25 22:55:01 -0800

    Build a basic engine and spawn tetrominos

--- Tetris/Model/TetrisGameModel.swift
+++ Tetris/Model/TetrisGameModel.swift
@@ -14,12 +14,16 @@ class TetrisGameModel: ObservableObject {
     @Published var gameBoard: [[TetrisGameBlock?]]
     @Published var tetromino: Tetromino?
     
+    var timer: Timer?
+    var speed: Double
+    
     init(numRows: Int = 23, numColumns: Int = 10) {
         self.numRows = numRows
         self.numColumns = numColumns
         
         gameBoard = Array(repeating: Array(repeating: nil, count: numRows), count: numColumns)
-        tetromino = Tetromino(origin: BlockLocation(row: 22, column: 4), blockType: .i)
+        speed = 0.1
+        resumeGame()
     }
     
     func blockClicked(row: Int, column: Int) {
@@ -29,6 +33,71 @@ class TetrisGameModel: ObservableObject {
         } else {
             gameBoard[column][row] = nil
         }
+    }
+    
+    func resumeGame() {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: speed, repeats: true, block: runEngine)
+    }
+    
+    func pauseGame() {
+        timer?.invalidate()
+    }
+    
+    func runEngine(timer: Timer) {
+        // Spawn a new block if we need to
+        guard let currentTetromino = tetromino else {
+            print("Spawning new Tetromino")
+            tetromino = Tetromino.createNewTetromino(numRows: numRows, numColumns: numColumns)
+            if !isValidTetromino(testTetromino: tetromino!) {
+                print("Game over!")
+                pauseGame()
+            }
+            return
+        }
+        
+        // See about moving block down
+        let newTetromino = currentTetromino.moveBy(row: -1, column: 0)
+        if isValidTetromino(testTetromino: newTetromino) {
+            print("Moving Tetromino down")
+            tetromino = newTetromino
+            return
+        }
+        
+        // see if we need to place the block
+        print("Placing tetromino")
+        placeTetromino()
+    }
+    
+    func isValidTetromino(testTetromino: Tetromino) -> Bool {
+        for block in testTetromino.blocks {
+            let row = testTetromino.origin.row + block.row
+            if row < 0 || row >= numRows { return false }
+            
+            let column = testTetromino.origin.column + block.column
+            if column < 0 || column >= numColumns { return false }
+            
+            if gameBoard[column][row] != nil { return false }
+        }
+        return true
+    }
+    
+    func placeTetromino() {
+        guard let currentTetromino = tetromino else {
+            return
+        }
+        
+        for block in currentTetromino.blocks {
+            let row = currentTetromino.origin.row + block.row
+            if row < 0 || row >= numRows { continue }
+            
+            let column = currentTetromino.origin.column + block.column
+            if column < 0 || column >= numColumns { continue }
+            
+            gameBoard[column][row] = TetrisGameBlock(blockType: currentTetromino.blockType)
+        }
+        
+        tetromino = nil
     }
     
 }
@@ -46,12 +115,43 @@ struct Tetromino {
     var blockType: BlockType
     
     var blocks: [BlockLocation] {
-        [
-            BlockLocation(row: 0, column: -1),
-            BlockLocation(row: 0, column: 0),
-            BlockLocation(row: 0, column: 1),
-            BlockLocation(row: 0, column: 2),
-        ]
+        return Tetromino.getBlocks(blockType: blockType)
+    }
+    
+    func moveBy(row: Int, column: Int) -> Tetromino {
+        let newOrigin = BlockLocation(row: origin.row + row, column: origin.column + column)
+        return Tetromino(origin: newOrigin, blockType: blockType)
+    }
+    
+    static func getBlocks(blockType: BlockType) -> [BlockLocation] {
+        switch blockType {
+        case .i:
+            return [BlockLocation(row: 0, column: -1), BlockLocation(row: 0, column: 0), BlockLocation(row: 0, column: 1), BlockLocation(row: 0, column: 2)]
+        case .o:
+            return [BlockLocation(row: 0, column: 0), BlockLocation(row: 0, column: 1), BlockLocation(row: 1, column: 1), BlockLocation(row: 1, column: 0)]
+        case .t:
+            return [BlockLocation(row: 0, column: -1), BlockLocation(row: 0, column: 0), BlockLocation(row: 0, column: 1), BlockLocation(row: 1, column: 0)]
+        case .j:
+            return [BlockLocation(row: 1, column: -1), BlockLocation(row: 0, column: -1), BlockLocation(row: 0, column: 0), BlockLocation(row: 0, column: 1)]
+        case .l:
+            return [BlockLocation(row: 0, column: -1), BlockLocation(row: 0, column: 0), BlockLocation(row: 0, column: 1), BlockLocation(row: 1, column: 1)]
+        case .s:
+            return [BlockLocation(row: 0, column: -1), BlockLocation(row: 0, column: 0), BlockLocation(row: 1, column: 0), BlockLocation(row: 1, column: 1)]
+        case .z:
+            return [BlockLocation(row: -1, column: 0), BlockLocation(row: 0, column: 0), BlockLocation(row: 0, column: -1), BlockLocation(row: -1, column: 1)]
+        }
+    }
+    
+    static func createNewTetromino(numRows: Int, numColumns: Int) -> Tetromino {
+        let blockType = BlockType.allCases.randomElement()!
+        
+        var maxRow = 0
+        for block in getBlocks(blockType: blockType) {
+            maxRow = max(maxRow, block.row)
+        }
+        
+        let origin = BlockLocation(row: numRows - 1 - maxRow, column: (numColumns-1)/2)
+        return Tetromino(origin: origin, blockType: blockType)
     }
 }
 
